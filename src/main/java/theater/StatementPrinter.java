80b2bb91f86849586ba9b94e672cb0107c5017ed
Hayato Kukihara <hayato.kukihara@mail.utoronto.ca>
2025-11-22 18:58:56 -0500

    final edit

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -81,8 +81,10 @@ public class StatementPrinter {
             case "tragedy":
                 result = Constants.TRAGEDY_BASE_AMOUNT;
                 if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
-                    result += Constants.HISTORY_OVER_BASE_CAPACITY_PER_PERSON * (performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD);
+                    result += Constants.HISTORY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD);
                 }
+
                 break;
             case "comedy":
                 result = Constants.COMEDY_BASE_AMOUNT;
